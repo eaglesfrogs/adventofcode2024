26a6687b11cb5759f11bdf70fe1607e411abf7ce
Steve Calder <steve.r.calder@gmail.com>
2024-12-01 09:16:44 -0500

    day 1 in the bag!

--- src/day1/step1.rs
+++ src/day1/step1.rs
@@ -1,7 +1,27 @@
 pub fn execute(data: &Vec<String>) {
-    println!("day 1 step 1");
+    let mut total: i32 = 0;
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_list: Vec<i32> = Vec::new();
 
-    for d in data {
-        println!("{}", d);
+    for line in data {
+        let first_num_string = &line[0..5];
+        let second_num_string = &line[8..13];
+        let first_num = first_num_string.parse::<i32>().unwrap();
+        let second_num = second_num_string.parse::<i32>().unwrap();
+
+        left_list.push(first_num);
+        right_list.push(second_num);
     }
+
+    left_list.sort();
+    right_list.sort();
+
+    for i in 0..left_list.len() {
+        let first_num = left_list[i];
+        let second_num = right_list[i];
+
+        total = &total + (first_num - second_num).abs();
+    }
+
+    println!("Step 1 Total {}", total);
 }
--- src/day1/step2.rs
+++ src/day1/step2.rs
@@ -1,7 +1,28 @@
-pub fn execute(data: &Vec<String>) {
-    println!("day 1 step 2");
+use std::collections::HashMap;
 
-    for d in data {
-        println!("{}", d);
+pub fn execute(data: &Vec<String>) {
+    let mut total: i32 = 0;
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_list_map: HashMap<i32, i32> = HashMap::new();
+
+    for line in data {
+        let first_num_string = &line[0..5];
+        let second_num_string = &line[8..13];
+        let first_num = first_num_string.parse::<i32>().unwrap();
+        let second_num = second_num_string.parse::<i32>().unwrap();
+
+        left_list.push(first_num);
+
+        let num_count = right_list_map.entry(second_num).or_insert(0);
+        *num_count += 1;
     }
+
+    for i in 0..left_list.len() {
+        let first_num = left_list[i];
+        let right_list_count = right_list_map.get(&first_num).copied().unwrap_or(0);
+
+        total = &total + &first_num * right_list_count;
+    }
+
+    println!("Step 2 Total {}", total);
 }
