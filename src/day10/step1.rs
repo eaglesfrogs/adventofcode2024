65a0d7a419839c5b38f04f3e39101577a344739d
Steve Calder <steve.r.calder@gmail.com>
2024-12-10 20:55:14 -0500

    ezpz10

--- src/day10/step1.rs
+++ src/day10/step1.rs
@@ -1,19 +1,51 @@
+use std::collections::HashSet;
+
 const ROWS_COLS: usize = 45;
+
+fn count_paths(point: (usize, usize), puzzle: &[[usize; 45]; 45]) -> HashSet<(usize, usize)> {
+    let (x, y) = point;
+    let val = puzzle[x][y];
+    let mut visited_points = HashSet::<(usize, usize)>::new();
+
+    if val == 9 {
+        visited_points.insert(point);
+
+        return visited_points;
+    }
+
+    if x > 0 && val + 1 == puzzle[x - 1][y] {
+        visited_points.extend(count_paths((x - 1, y), puzzle).into_iter());
+    }
+
+    if y > 0 && val + 1 == puzzle[x][y - 1] {
+        visited_points.extend(count_paths((x, y - 1), puzzle).into_iter());
+    }
+
+    if x < ROWS_COLS - 1 && val + 1 == puzzle[x + 1][y] {
+        visited_points.extend(count_paths((x + 1, y), puzzle).into_iter());
+    }
+
+    if y < ROWS_COLS - 1 && val + 1 == puzzle[x][y + 1] {
+        visited_points.extend(count_paths((x, y + 1), puzzle).into_iter());
+    }
+
+    visited_points
+}
 
 pub fn execute(data: &Vec<String>) {
     let mut puzzle = [[0; ROWS_COLS]; ROWS_COLS];
-    let mut total: i32 = 0;
+    let mut total: usize = 0;
 
-    let mut starting_points = Vec::<(u32, u32)>::new();
+    let mut starting_points = Vec::<(usize, usize)>::new();
 
-    let mut i: u32 = 0;
+    let mut i: usize = 0;
 
     for line in data {
-        let mut j: u32 = 0;
+        let mut j: usize = 0;
 
         for c in line.chars() {
             let d: u32 = c.to_digit(10).unwrap();
-            puzzle[i][j] = d;
+            puzzle[i][j] = d as usize;
 
             if d == 0 {
                 starting_points.push((i, j));
@@ -22,6 +54,12 @@ pub fn execute(data: &Vec<String>) {
             j += 1;
         }
         i += 1;
+    }
+
+    while starting_points.len() > 0 {
+        let starting_point = starting_points.pop().unwrap();
+
+        total += count_paths(starting_point, &puzzle).len();
     }
 
     println!("Step 1 Total {}", total);
--- src/day10/step2.rs
+++ src/day10/step2.rs
@@ -1,21 +1,61 @@
 const ROWS_COLS: usize = 45;
 
+fn count_paths(point: (usize, usize), puzzle: &[[usize; 45]; 45]) -> usize {
+    let (x, y) = point;
+    let val = puzzle[x][y];
+    let mut count = 0;
+
+    if val == 9 {
+        return 1;
+    }
+
+    if x > 0 && val + 1 == puzzle[x - 1][y] {
+        count += count_paths((x - 1, y), puzzle);
+    }
+
+    if y > 0 && val + 1 == puzzle[x][y - 1] {
+        count += count_paths((x, y - 1), puzzle);
+    }
+
+    if x < ROWS_COLS - 1 && val + 1 == puzzle[x + 1][y] {
+        count += count_paths((x + 1, y), puzzle);
+    }
+
+    if y < ROWS_COLS - 1 && val + 1 == puzzle[x][y + 1] {
+        count += count_paths((x, y + 1), puzzle);
+    }
+
+    count
+}
+
 pub fn execute(data: &Vec<String>) {
     let mut puzzle = [[0; ROWS_COLS]; ROWS_COLS];
-    let mut total: i32 = 0;
+    let mut total: usize = 0;
 
-    let mut i = 0;
+    let mut starting_points = Vec::<(usize, usize)>::new();
+
+    let mut i: usize = 0;
 
     for line in data {
-        let mut j = 0;
+        let mut j: usize = 0;
 
         for c in line.chars() {
             let d: u32 = c.to_digit(10).unwrap();
-            puzzle[i][j] = d;
+            puzzle[i][j] = d as usize;
+
+            if d == 0 {
+                starting_points.push((i, j));
+            }
 
             j += 1;
         }
         i += 1;
+    }
+
+    while starting_points.len() > 0 {
+        let starting_point = starting_points.pop().unwrap();
+
+        total += count_paths(starting_point, &puzzle);
     }
 
     println!("Step 1 Total {}", total);
