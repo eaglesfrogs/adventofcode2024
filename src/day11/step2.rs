30896b59dcc77c5e79513c4f6550144ba366a87f
Steve Calder <scalder@clarityinnovates.com>
2024-12-11 19:00:42 -0500

    day 11 part 2

--- src/day11/step2.rs
+++ src/day11/step2.rs
@@ -1,3 +1,7 @@
+use cached::proc_macro::cached;
+
+const MAX_ITERATIONS: u32 = 75;
+
 fn get_num_digits(mut num: u64) -> i32 {
     if num == 0 {
         return 1;
@@ -13,6 +17,34 @@ fn get_num_digits(mut num: u64) -> i32 {
     count
 }
 
+#[cached]
+fn formulate(stone: u64, iteration: u32) -> u64 {
+    if iteration == MAX_ITERATIONS {
+        return 1;
+    }
+
+    let mut total: u64 = 0;
+
+    if stone == 0 {
+        total += formulate(1 as u64, iteration + 1);
+    } else {
+        let num_digits = get_num_digits(stone);
+
+        if num_digits % 2 == 0 {
+            let tens = (10 as u64).pow((num_digits / 2) as u32);
+            let left: u64 = stone / tens;
+            let right: u64 = stone % tens;
+
+            total += formulate(left, iteration + 1);
+            total += formulate(right, iteration + 1);
+        } else {
+            total += formulate(stone * 2024, iteration + 1);
+        }
+    }
+
+    return total;
+}
+
 pub fn execute(data: &Vec<String>) {
     let mut stones: Vec<u64> = Vec::<u64>::new();
 
@@ -24,32 +56,11 @@ pub fn execute(data: &Vec<String>) {
         }
     }
 
-    for _i in 0..75 {
-        let mut next_stones = Vec::<u64>::new();
+    let mut total: u64 = 0;
 
-        for stone in stones {
-            if stone == 0 {
-                next_stones.push(1);
-            } else {
-                let num_digits = get_num_digits(stone);
-
-                if num_digits % 2 == 0 {
-                    let tens = (10 as u64).pow((num_digits / 2) as u32);
-                    let left: u64 = stone / tens;
-                    let right: u64 = stone % tens;
-
-                    next_stones.push(left);
-                    next_stones.push(right);
-                } else {
-                    next_stones.push(stone * 2024);
-                }
-            }
-        }
-
-        println!("{}", next_stones.len());
-
-        stones = next_stones;
+    for stone in stones {
+        total += formulate(stone, 0);
     }
 
-    println!("Step 2 Total {}", stones.len());
+    println!("Step 2 Total {}", total);
 }
